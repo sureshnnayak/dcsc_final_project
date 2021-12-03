9fde5cf68e0c5638193a675894dafd6a82f3e7d6
anusha97 <anusha.basavaraja@colorado.edu>
2021-12-03 16:36:27 -0700

    updated deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #all deploy commands
-kubectl apply -f
-kubectl apply -f
-kubectl apply -f
-kubectl apply -f
-kubectl apply -f
+kubectl apply -f redis/redis-deployment.yaml
+kubectl apply -f redis/redis-service.yaml
+kubectl apply -f rabbitmq/rabbitmq-deployment.yaml
+kubectl apply -f rabbitmq/rabbitmq-service.yaml
+
+kubectl apply -f rest/rest-deployment.yaml
+kubectl apply -f rest/rest-service.yaml
+
+kubectl apply -f logs/logs-deployment.yaml
+
+kubectl apply -f worker/worker-deployment.yaml
